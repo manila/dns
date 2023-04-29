407628a9345ea92ef0495fcc4df9c294c40a37eb
manila <3621543+manila@users.noreply.github.com>
2023-04-29 02:52:47 -0400

    manila.me: Add MX record

--- manila.me.tf
+++ manila.me.tf
@@ -62,3 +62,48 @@ resource "cloudflare_record" "manila_me_aaaa_3" {
   ttl     = 1
   value   = "2606:50c0:8003::153"
 }
+
+resource "cloudflare_record" "manila_me_mx_0" {
+  zone_id  = var.cloudflare_zone_id
+  name     = "manila.me"
+  type     = "MX"
+  ttl      = 1
+  value    = "aspmx.l.google.com"
+  priority = 1
+}
+
+resource "cloudflare_record" "manila_me_mx_1" {
+  zone_id  = var.cloudflare_zone_id
+  name     = "manila.me"
+  type     = "MX"
+  ttl      = 1
+  value    = "alt1.aspmx.l.google.com"
+  priority = 5
+}
+
+resource "cloudflare_record" "manila_me_mx_2" {
+  zone_id  = var.cloudflare_zone_id
+  name     = "manila.me"
+  type     = "MX"
+  ttl      = 1
+  value    = "alt2.aspmx.l.google.com"
+  priority = 5
+}
+
+resource "cloudflare_record" "manila_me_mx_3" {
+  zone_id  = var.cloudflare_zone_id
+  name     = "manila.me"
+  type     = "MX"
+  ttl      = 1
+  value    = "alt3.aspmx.l.google.com"
+  priority = 10
+}
+
+resource "cloudflare_record" "manila_me_mx_4" {
+  zone_id  = var.cloudflare_zone_id
+  name     = "manila.me"
+  type     = "MX"
+  ttl      = 1
+  value    = "alt4.aspmx.l.google.com"
+  priority = 10
+}
