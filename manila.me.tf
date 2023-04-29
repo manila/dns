e43ddca8faa15a218d2219188b3c7ec5521f903d
manila <3621543+manila@users.noreply.github.com>
2023-04-29 02:57:01 -0400

    manila.me: Add DMARC record

--- manila.me.tf
+++ manila.me.tf
@@ -63,6 +63,14 @@ resource "cloudflare_record" "manila_me_aaaa_3" {
   value   = "2606:50c0:8003::153"
 }
 
+resource "cloudflare_record" "manila_me_txt_dmarc" {
+  zone_id = var.cloudflare_zone_id
+  name    = "_dmarc"
+  type    = "TXT"
+  ttl     = 1
+  value   = "v=DMARC1; p=none; rua=mailto:dmarc@manila.me"
+}
+
 resource "cloudflare_record" "manila_me_mx_0" {
   zone_id  = var.cloudflare_zone_id
   name     = "manila.me"
